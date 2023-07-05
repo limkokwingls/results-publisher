bccb2a44c2032b62c87f6d80b3a4ba7d6ddd1aab
Ntholi Nkhatho <ntholi@live.com>
2023-07-05 09:10:36 +0200

    make log little bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center'>
       <div className='flex flex-col items-center justify-center'>
-        <h1 className='text-5xl font-thin text-center mt-14 mb-5'>
+        <h1 className='text-4xl font-bold text-zinc-300 text-center mt-14 mb-2'>
           Result Publication
         </h1>
-        <Image alt='Logo' src='/logo.png' width={300} height={200} />
+        <Image alt='Logo' src='/logo.png' width={325} height={200} />
       </div>
       <div className='grid text-center mt-10 lg:mb-0 gap-5 lg:grid-cols-3 lg:text-left'>
-        <Clickable href='#' text='Degree Programs' />
-        <Clickable href='#' text='Diploma Programs' />
-        <Clickable href='#' text='Certificate Programs' />
+        <Clickable href='/programs?level=degree' text='Degree Programs' />
+        <Clickable href='/programs?level=degree' text='Diploma Programs' />
+        <Clickable href='/programs?level=degree' text='Certificate Programs' />
       </div>
     </main>
   );
