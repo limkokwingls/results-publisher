793c00b2becd97fdd4a5b5ca7e7d37d75d391c5f
Ntholi Nkhatho <ntholi@live.com>
2023-07-05 09:02:57 +0200

    display logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,14 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='mb-32 grid text-center lg:mb-0 gap-5 lg:grid-cols-3 lg:text-left'>
+    <main className='flex min-h-screen flex-col items-center'>
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='text-5xl font-thin text-center mt-14 mb-5'>
+          Result Publication
+        </h1>
+        <Image alt='Logo' src='/logo.png' width={300} height={200} />
+      </div>
+      <div className='grid text-center mt-10 lg:mb-0 gap-5 lg:grid-cols-3 lg:text-left'>
         <Clickable href='#' text='Degree Programs' />
         <Clickable href='#' text='Diploma Programs' />
         <Clickable href='#' text='Certificate Programs' />
