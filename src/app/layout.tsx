e1c45c6432db25328011e6371f23087f96d28653
Ntholi Nkhatho <ntholi@live.com>
2023-07-05 17:43:01 +0200

    move common tags to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Image from 'next/image';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='flex min-h-screen flex-col items-center'>
+          <div className='flex flex-col items-center justify-center'>
+            <h1 className='text-4xl font-bold text-zinc-300 text-center mt-14 mb-2'>
+              Result Publication
+            </h1>
+            <Image alt='Logo' src='/logo.png' width={325} height={200} />
+          </div>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
 import Clickable from '@/components/Clickable';
-import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center'>
-      <div className='flex flex-col items-center justify-center'>
-        <h1 className='text-4xl font-bold text-zinc-300 text-center mt-14 mb-2'>
-          Result Publication
-        </h1>
-        <Image alt='Logo' src='/logo.png' width={325} height={200} />
-      </div>
-      <div className='grid text-center mt-10 lg:mb-0 gap-5 lg:grid-cols-3 lg:text-left'>
-        <Clickable href='/programs?level=degree' text='Degree Programs' />
-        <Clickable href='/programs?level=degree' text='Diploma Programs' />
-        <Clickable href='/programs?level=degree' text='Certificate Programs' />
-      </div>
-    </main>
+    <div className='grid text-center mt-10 lg:mb-0 gap-5 lg:grid-cols-3 lg:text-left'>
+      <Clickable href='/programs?level=Degree' text='Degree Programs' />
+      <Clickable href='/programs?level=Diploma' text='Diploma Programs' />
+      <Clickable
+        href='/programs?level=Certificate'
+        text='Certificate Programs'
+      />
+    </div>
   );
 }
