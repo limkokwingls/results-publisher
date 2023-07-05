1fa58c075fdec640bf7c12612d253e4751d95c8e
Ntholi Nkhatho <ntholi@live.com>
2023-07-05 20:19:02 +0200

    display programs on hompage

---
+++ src/app/faculties/[id]/page.tsx
@@ -0,0 +1,27 @@
+import Clickable from '@/components/Clickable';
+import { prisma } from '@/lib/db';
+
+type Props = { params: { id: string } };
+
+export default async function ProgramsPage({ params }: Props) {
+  const id = Number(params.id);
+  const programs = await prisma.program.findMany({
+    where: {
+      faculty_id: id,
+    },
+  });
+
+  return (
+    <ul className='grid text-center mt-10 lg:mb-0 gap-5 lg:grid-cols-3 lg:text-left'>
+      {programs.map((program) => (
+        <li key={program.id}>
+          <Clickable
+            key={program.id}
+            href={`/programs/${program.id}`}
+            text={program.name}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import Clickable from '@/components/Clickable';
 import { prisma } from '@/lib/db';
 
 export default async function Home() {
-  // const programs = await prisma.programs.
+  const faculties = await prisma.faculty.findMany();
   return (
     <div className='grid text-center mt-10 lg:mb-0 gap-5 lg:grid-cols-3 lg:text-left'>
-      <Clickable href='/programs?level=Degree' text='Degree Programs' />
-      <Clickable href='/programs?level=Diploma' text='Diploma Programs' />
-      <Clickable
-        href='/programs?level=Certificate'
-        text='Certificate Programs'
-      />
+      {faculties.map((faculty) => (
+        <Clickable
+          key={faculty.id}
+          href={`/faculties/${faculty.id}`}
+          text={faculty.name}
+        />
+      ))}
     </div>
   );
 }
