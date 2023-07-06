5713843182af40efb85fd527f7d30fa9ea1f322b
Ntholi Nkhatho <ntholi@live.com>
2023-07-06 15:57:01 +0200

    Don't display blocked results

--- src/app/students/[id]/page.tsx
+++ src/app/students/[id]/page.tsx
@@ -20,41 +20,47 @@ export default async function FacultyPage({ params }: Props) {
     <div className='mt-10 text-gray-200'>
       <div>
         <h2 className='text-xl font-semibold'>{stdNo}</h2>
-        <span className='text-xs mt-2 block px-2 py-4 bg-zinc-900  border border-zinc-800'>
-          {remarks?.remarks || 'No Remarks'}
-        </span>
+        <div className='text-xs mt-2 px-2 py-4 bg-zinc-900  border border-zinc-800'>
+          {remarks?.is_blocked ? (
+            <span>Blocked</span>
+          ) : (
+            <span>{remarks?.remarks || 'No Remarks'}</span>
+          )}
+        </div>
       </div>
-      <div className='relative overflow-x-auto mt-6'>
-        <table className='w-full mt-5 text-sm text-left'>
-          <thead className='border-b border-gray-400 font-semibold tracking-wider '>
-            <th scope='col' className='pb-3 text-left'>
-              Course
-            </th>
-            <th scope='col' className='pb-3 text-left'>
-              Grade
-            </th>
-            <th scope='col' className='pb-3 text-left'>
-              Marks
-            </th>
-            <th scope='col' className='pb-3 text-left'>
-              Points
-            </th>
-          </thead>
-          <tbody>
-            {grades.map((grade) => (
-              <tr key={grade.id}>
-                <td className='py-2 pe-4' scope='row'>
-                  <p>{grade.name}</p>
-                  <p className='text-xs text-zinc-400'>{grade.code}</p>
-                </td>
-                <td className='py-2 px-4'>{grade.grade}</td>
-                <td className='py-2 px-4'>{grade.marks}</td>
-                <td className='py-2 px-4'>{grade.points}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {!remarks?.is_blocked && (
+        <div className='relative overflow-x-auto mt-6'>
+          <table className='w-full mt-5 text-sm text-left'>
+            <thead className='border-b border-gray-400 font-semibold tracking-wider '>
+              <th scope='col' className='pb-3 text-left'>
+                Course
+              </th>
+              <th scope='col' className='pb-3 text-left'>
+                Grade
+              </th>
+              <th scope='col' className='pb-3 text-left'>
+                Marks
+              </th>
+              <th scope='col' className='pb-3 text-left'>
+                Points
+              </th>
+            </thead>
+            <tbody>
+              {grades.map((grade) => (
+                <tr key={grade.id}>
+                  <td className='py-2 pe-4' scope='row'>
+                    <p>{grade.name}</p>
+                    <p className='text-xs text-zinc-400'>{grade.code}</p>
+                  </td>
+                  <td className='py-2 px-4'>{grade.grade}</td>
+                  <td className='py-2 px-4'>{grade.marks}</td>
+                  <td className='py-2 px-4'>{grade.points}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
