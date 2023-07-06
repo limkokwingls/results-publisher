343f994fcc639f833720ebcb6734b94c2fd5d196
Ntholi Nkhatho <ntholi@live.com>
2023-07-06 17:30:02 +0200

    handle student number search

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,6 +1,17 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function SearchField() {
+  const [studentNo, setStudentNo] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    window.location.href = `/students/${studentNo}`;
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='relative'>
         <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
           <svg
@@ -24,6 +35,8 @@ export default function SearchField() {
           id='default-search'
           className='block w-full p-3 pl-10 text-sm border rounded-lg bg-inherit border-zinc-700 placeholder-gray-400 text-white focus:ring-gray-500 focus:border-gray-500'
           placeholder='Student Number'
+          value={studentNo}
+          onChange={(e) => setStudentNo(e.target.value)}
           required
         />
         <button
