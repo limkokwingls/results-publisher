354065e3f24c9c3d1627eaea35137718bdcf0cf5
Ntholi Nkhatho <ntholi@live.com>
2023-07-06 21:09:28 +0200

    reduce padding in Clickable for smaller screens

--- src/components/Clickable.tsx
+++ src/components/Clickable.tsx
@@ -10,7 +10,7 @@ export default function Clickable({ href, children }: Props) {
   return (
     <Link
       href={`${href}`}
-      className='group rounded-lg border border-zinc-900 px-10 py-8 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
+      className='group rounded-lg border border-zinc-900 px-8 md:px-10 py-6 md:py-8 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
     >
       <h2 className={'flex gap-3 items-center'}>
         <div>{children}</div>
