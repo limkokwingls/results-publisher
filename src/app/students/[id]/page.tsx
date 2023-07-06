3f78b6b55a3074cd125ca9b94db1a47ab0906621
Ntholi Nkhatho <ntholi@live.com>
2023-07-06 10:03:41 +0200

    Style table

--- src/app/students/[id]/page.tsx
+++ src/app/students/[id]/page.tsx
@@ -14,27 +14,37 @@ export default async function FacultyPage({ params }: Props) {
   return (
     <div className='mt-10 text-gray-200'>
       <h2>Results for {stdNo}</h2>
-      <table className='w-full mt-5'>
-        <thead className='border-b border-gray-400 font-semibold tracking-wider '>
-          <th className='pb-3 text-left'>Course</th>
-          <th className='pb-3 text-left'>Grade</th>
-          <th className='pb-3 text-left'>Marks</th>
-          <th className='pb-3 text-left'>Points</th>
-        </thead>
-        <tbody>
-          {grades.map((grade) => (
-            <tr key={grade.id}>
-              <td className='py-2'>
-                <p>{grade.name}</p>
-                <p className='text-xs text-zinc-400'>{grade.code}</p>
-              </td>
-              <td className='py-2'>{grade.grade}</td>
-              <td className='py-2'>{grade.marks}</td>
-              <td className='py-2'>{grade.points}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div className='relative overflow-x-auto'>
+        <table className='w-full mt-5 text-sm text-left'>
+          <thead className='border-b border-gray-400 font-semibold tracking-wider '>
+            <th scope='col' className='pb-3 text-left'>
+              Course
+            </th>
+            <th scope='col' className='pb-3 text-left'>
+              Grade
+            </th>
+            <th scope='col' className='pb-3 text-left'>
+              Marks
+            </th>
+            <th scope='col' className='pb-3 text-left'>
+              Points
+            </th>
+          </thead>
+          <tbody>
+            {grades.map((grade) => (
+              <tr key={grade.id}>
+                <td className='py-2 pe-4' scope='row'>
+                  <p>{grade.name}</p>
+                  <p className='text-xs text-zinc-400'>{grade.code}</p>
+                </td>
+                <td className='py-2 px-4'>{grade.grade}</td>
+                <td className='py-2 px-4'>{grade.marks}</td>
+                <td className='py-2 px-4'>{grade.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
