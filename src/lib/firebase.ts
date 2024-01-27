875638c7834364db4c60e4e899aed27e4961c354
Ntholi Nkhatho <ntholi@live.com>
2024-01-27 07:41:17 +0200

    Update Firebase configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyA90sg8_g0WZpmuXt5v_8i-j_454aI0X-Q',
-  authDomain: 'viewmyresults.firebaseapp.com',
-  projectId: 'viewmyresults',
-  storageBucket: 'viewmyresults.appspot.com',
-  messagingSenderId: '632024792802',
-  appId: '1:632024792802:web:3e8504e3e9d2d96e2fa8bb',
+  apiKey: 'AIzaSyDGDhocALl0PXVFnMsj1PCi4zOhvR-gifI',
+  authDomain: 'viewresultsonline.firebaseapp.com',
+  projectId: 'viewresultsonline',
+  storageBucket: 'viewresultsonline.appspot.com',
+  messagingSenderId: '577651192345',
+  appId: '1:577651192345:web:abde2994e765c8ed0b7088',
 };
 
 // Initialize Firebase
