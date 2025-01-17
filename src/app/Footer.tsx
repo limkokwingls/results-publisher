46b8b3570ca6f93730c1e04c5301a2f69a9be34b
Ntholi Nkhatho <ntholi@live.com>
2025-01-17 13:46:36 +0200

    Update footer link to point to the correct GitHub repository

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className='flex justify-end text-white bg-black/20 md:p-3 mt-10'>
       <Link
-        href={'https://github.com/ntholi/results-publisher'}
+        href={'https://github.com/limkokwingls/results-publisher'}
         className='flex items-center gap-2 text-muted-foreground'
         target='_blank'
       >
