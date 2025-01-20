bb05c78b23a821d3562ea6a3f24b5926f646dbb0
Ntholi Nkhatho <ntholi@live.com>
2025-01-20 07:56:18 +0200

    Refactor unblock function to strip whitespace from student numbers and improve error handling

--- backend/unblock.py
+++ backend/unblock.py
@@ -1,12 +1,17 @@
+import os
 from typing import List, NoReturn
+
+from db import cleanup_db, get_db
 from googleapiclient.discovery import Resource
 from openpyxl.worksheet.worksheet import Worksheet
+from rich import print
+from spreadsheet import (
+    get_blocked_students,
+    get_google_sheets_service,
+    mark_as_unblocked,
+)
 from tqdm import tqdm
 
-from db import get_db, cleanup_db
-from spreadsheet import get_blocked_students, mark_as_unblocked, get_google_sheets_service
-from rich import print
-import os
 
 def unblock(student_number: str) -> None:
     app, db = get_db()
@@ -16,25 +21,26 @@ def unblock(student_number: str) -> None:
     cleanup_db(app)
 
 
-def main() -> NoReturn:
+def main():
     """Unblock students and update their status in both database and spreadsheet."""
     try:
-      print("Initializing...")
-      service = get_google_sheets_service()
-      blocked_students = get_blocked_students(service)
-      
-      if not blocked_students:
-          print("No blocked students found.")
-      else:
-        print(f"Found {len(blocked_students)} blocked students.")
-        for student in tqdm(blocked_students, desc="Unblocking students"):
-            unblock(student)
-            mark_as_unblocked(service, student)
-        print("\nDone!")
+        print("Initializing...")
+        service = get_google_sheets_service()
+        blocked_students = get_blocked_students(service)
+
+        if not blocked_students:
+            print("No blocked students found.")
+        else:
+            print(f"Found {len(blocked_students)} blocked students.")
+            for student in tqdm(blocked_students, desc="Unblocking students"):
+                unblock(student.strip())
+                mark_as_unblocked(service, student)
+            print("\nDone!")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-    
+
     os.system("pause")
+
 
 if __name__ == "__main__":
     main()
